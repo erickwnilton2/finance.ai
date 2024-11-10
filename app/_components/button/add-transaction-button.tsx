3f9607c1ca5a6810c34eb08b733cc2b3900c6aef
erick <erickwelberth1@hotmail.com>
2024-11-09 23:29:38 -0300

    feat: create button component

---
+++ app/_components/button/add-transaction-button.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+import { Input } from "../ui/input";
+import { MoneyInput } from "../input/money-input";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome é obrigatório",
+  }),
+  amout: z.string().trim().min(1, {
+    message: "O valor é obrigatório",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatório",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória",
+  }),
+});
+
+const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amout: "",
+      category: TransactionCategory.OTHER,
+      date: new Date(),
+      name: "",
+      paymentMethod: TransactionPaymentMethod.CASH,
+      type: TransactionType.EXPENSE,
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nome da transação" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amout"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <MoneyInput placeholder="Valor da transação" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button variant="outline" type="button">
+                Cancelar
+              </Button>
+              <Button type="submit">Adicionar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTransactionButton;
