23c76abdf4fecd29ce6e9e920622c426fd2f8f9a
erick <erickwelberth1@hotmail.com>
2024-12-02 20:04:55 -0300

    style: page adding mobile devices first

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,28 +14,30 @@ const LoginPage = async () => {
 
   return (
     <div className="grid h-full grid-cols-2">
-      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image
-          src="/financeAI.svg"
-          width={173}
-          height={39}
-          alt="Finance AI"
-          className="mb-8"
-        />
-        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
-        </p>
-        <SignInButton>
-          <Button variant="outline">
-            <LogInIcon />
-            Fazer login ou criar conta
-          </Button>
-        </SignInButton>
+      <div className="fixed m-auto h-full items-center justify-center lg:relative">
+        <div className="m-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+          <Image
+            src="/financeAI.svg"
+            width={173}
+            height={39}
+            alt="Finance AI"
+            className="mb-8"
+          />
+          <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+          <p className="mb-8 text-muted-foreground">
+            A Finance AI é uma plataforma de gestão financeira que utiliza IA
+            para monitorar suas movimentações, e oferecer insights
+            personalizados, facilitando o controle do seu orçamento.
+          </p>
+          <SignInButton>
+            <Button variant="outline">
+              <LogInIcon />
+              Fazer login ou criar conta
+            </Button>
+          </SignInButton>
+        </div>
       </div>
-      <div className="relative h-full w-full">
+      <div className="relative hidden h-full w-full lg:flex">
         <Image src="/login.png" alt="login" fill className="object-cover" />
       </div>
     </div>
